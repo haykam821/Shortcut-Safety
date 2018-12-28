cafb7811d1803a43d4a182ea7ec67077f957ab6f
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-12-27 21:50:27 -0500

    Add ShortcutSafetyRating

--- src/index.js
+++ src/index.js
@@ -5,6 +5,33 @@ const noopActions = Object.keys(actions).filter(key => {
 	return actions[key] === 0;
 });
 
+class ShortcutSafetyRating {
+	constructor(rank, actions, metadata) {
+		/**
+		 * The safety ranking of the shortcut.
+		 * A lower number is better.
+		 */
+		this.rank = rank || 0;
+
+		/**
+		 * An object mapping unsafe actions in the shortcut to their severity.
+		 */
+		this.actions = actions || {};
+
+		/**
+		 * The tested shortcut metadata.
+		 * @type {ShortcutMetadata}
+		 */
+		this.metadata = metadata;
+	}
+}
+module.exports.ShortcutSafetyRating = ShortcutSafetyRating;
+
+/**
+ * Gets the safety of a shortcut.
+ * @param {ShortcutMetadata} shortcutMetadata The metadata of a shortcut to get the safety of.
+ * @returns {ShortcutSafetyRating}
+ */
 function shortcutSafety(shortcutMetadata) {
 	if (shortcutMetadata === undefined) {
 		throw new TypeError("The shortcutMetadata parameter is missing.");
@@ -18,12 +45,16 @@ function shortcutSafety(shortcutMetadata) {
 	const safetyTotal = nonNoopActions.reduce((prev, item) => {
 		const safetyOfCurrentAction = actions[item.identifier];
 		if (safetyOfCurrentAction) {
-			return prev + safetyOfCurrentAction;
+			prev[1][item.identifier] = safetyOfCurrentAction;
+			return [
+				prev[0] + safetyOfCurrentAction,
+				prev[1],
+			];
 		} else {
 			return prev;
 		}
-	}, 0)
+	}, [0, {}]);
 	
-	return safetyTotal / nonNoopActions.length;
+	return new ShortcutSafetyRating(safetyTotal[0] / nonNoopActions.length, safetyTotal[1], shortcutMetadata);
 }
-module.exports = shortcutSafety;
+module.exports.getShortcutSafety = shortcutSafety;
